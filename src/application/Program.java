4c59564af0840258704c89a0b5406367b7c1a382
Foedes Lucio Goncalves <foedesluciogoncalves@gmail.com>
2026-01-28 09:53:19 -0400

    criando entidade vendedor

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,20 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Departamento;
+import model.entities.Vendedor;
 
 public class Program {
 
 	public static void main(String[] args) {
 		Departamento depto = new Departamento(1,"livros");
+		Vendedor vendedor = new Vendedor(1,"pedro","pedro@gmail.com", new Date(),2000.0,depto);
+		
+		
 		System.out.println(depto);
+		System.out.println(vendedor);
+
 
 	}
 
