11cc5974c287200d9f04734082ca350ec237cf26
Foedes Lucio Goncalves <foedesluciogoncalves@gmail.com>
2026-01-28 09:19:47 -0400

    criando entidade departamento

---
+++ src/application/Program.java
@@ -0,0 +1,13 @@
+package application;
+
+import model.entities.Departamento;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Departamento depto = new Departamento(1,"livros");
+		System.out.println(depto);
+
+	}
+
+}
